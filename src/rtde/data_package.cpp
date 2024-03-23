362c9a4cb4475e977591951c638da0286ad518a2
dheena-rb23 <dheenadhayalan.r@rightbot.in>
2024-03-23 23:31:38 +0530

    Read set payload and set cog from output recipe

--- src/rtde/data_package.cpp
+++ src/rtde/data_package.cpp
@@ -554,6 +554,8 @@ std::unordered_map<std::string, DataPackage::_rtde_type_variant> DataPackage::g_
   { "standard_analog_output_0", double() },
   { "standard_analog_output_1", double() },
   { "tcp_offset", vector6d_t() },
+  { "payload", double() },
+  { "payload_cog", vector3d_t() },
 };
 
 void rtde_interface::DataPackage::initEmpty()
